d367628dd5a79864964cbad5bbd9675904a17829
Skye Pooley <tpb6375@autuni.ac.nz>
2024-08-28 21:55:17 +1200

    Integrated Linked Scene Tree with Godot
    Converted LevelTree to interface as I am planning to write a different implementation of leveltree soon.
    
    Created and tested a linked level tree class.
    Level tree loads the links between levels from a config file "level_links.dat"
    Level tree then holds the path of the current level and allows navigation between neighbouring levels.
    
    Each level can now have multiple exits referred to as children.
    Each level can now include a link to the previous level.

--- Godot Project/Scenes/LevelTree.cs
+++ Godot Project/Scenes/LevelTree.cs
@@ -1,57 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 
-namespace EscapeTheColossus.Scenes;
-
-public class LevelTree
+public interface LevelTree
 {
-    // Store scene nodes privately and expose paths through read-only properties.
-    private readonly PathNode _rootScene;
-    public string RootScenePath
-    {
-        get { return _rootScene.ScenePath; }
-    }
+    string RootScenePath { get; }
+    string CurrentScenePath { get; }
     
-    private PathNode _currentScene;
-    public string CurrentScenePath
-    {
-        get { return _currentScene.ScenePath; }
-    }
-
-    /// <summary>
-    /// Move to the current level's parent level if a parent level exists.
-    /// If this is the root node then the scene will not change.
-    /// </summary>
-    /// <returns>Newly selected scene path.</returns>
-    public string TraverseUp()
-    {
-        if (_currentScene.Parent != null)
-            _currentScene = _currentScene.Parent;
-        else
-            Console.WriteLine("No parent exists.");
-        return CurrentScenePath;
-    }
-
-    /// <summary>
-    /// Switch the current scene to a child of the current scene.
-    /// Throws IndexOutOfBoundsException if requested child does not exist.
-    /// </summary>
-    /// <param name="childIndex">Index of child scene.</param>
-    /// <returns>Path to newly selected scene.</returns>
-    public string TraverseToChild(int childIndex)
-    {
-        // Check requested child exists.
-        if (_currentScene.Children.Count < childIndex - 1)
-            throw new IndexOutOfRangeException();
-        // Switch to requested child node.
-        _currentScene = _currentScene.Children[childIndex];
-        return CurrentScenePath;
-    }
-}
-
-internal class PathNode(string path, PathNode parent)
-{
-    public string ScenePath { get; } = path;
-    public PathNode Parent { get; } = parent;
-    public List<PathNode> Children { get; set; }
+    string TraverseUp();
+    string TraverseToChild(int childIndex);
+    void Serialise(StreamWriter outputStream);
 }
