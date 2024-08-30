d6049e915b9074033c39a70008e8c6a3082749e0
Hendrik-Kruger <hendrik.b.k75@gmail.com>
2024-08-30 17:44:35 +1200

    Revert "Merge push"
    This reverts commit d12d91e9890adf82120a9d5b7d169f2ac970353b.

--- Godot Project/Scenes/Entities/RibBug/rib_bug.gd
+++ Godot Project/Scenes/Entities/RibBug/rib_bug.gd
@@ -7,10 +7,7 @@ var health = 2
 var direction = -1
 var player_inattack_zone = false 
 var can_take_damage = true
-var can_attack = false
-var is_alive = true
 
-@onready var attack_cooldown = $attack_cooldown
 @onready var take_damage_cooldown = $take_damage_cooldown
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var ray_cast_right = $RayCastRight
@@ -19,14 +16,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	deal_with_damage()
-	attack()
-	enemy()
-	
-	if health <= 0:
-		is_alive = false
-		animated_sprite.play("Death ")
-		if is_alive == false:
-			self.queue_free()
+		
 	if player_chase:
 			
 		if (direction) < 0:
@@ -49,7 +39,6 @@ func _physics_process(delta):
 		animated_sprite.play("Idle")
 	
 	move_and_slide()
-
 func _on_detection_body_entered(body):
 	player = body
 	player_chase = true
@@ -62,24 +51,10 @@ func _on_detection_body_exited(body):
 func _on_hit_box_body_entered(body):
 	if body.has_method("player"):
 		player_inattack_zone =true 
-		can_attack =true
 
 func _on_hit_box_body_exited(body):
 	if body.has_method("player"):
 		player_inattack_zone = false
-		can_attack = false
-		
-func attack():
-	var dir = direction
-	if player_inattack_zone and can_attack:
-		if dir == 1:
-			animated_sprite.flip_h = true
-			animated_sprite.play("Attack")
-			attack_cooldown.start()
-		if dir == -1:
-			animated_sprite.flip_h = false
-			animated_sprite.play("Attack")
-			attack_cooldown.start()
 		
 func deal_with_damage():
 	if player_inattack_zone and globall.player_current_attack == true:
@@ -87,13 +62,9 @@ func deal_with_damage():
 			health = health - 1
 			take_damage_cooldown.start()
 			can_take_damage = false
-			print(" health =", health)
+			print("slime health =", health)
 		
 func _on_take_damage_cooldown_timeout():
 	can_take_damage = true
-	
-func _on_attack_cooldown_timeout():
-	can_attack = true
-	
 func enemy():
 	pass
