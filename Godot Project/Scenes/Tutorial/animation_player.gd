3d2be17f0512a4d973451675ece5fd546bd0f9c5
godofocean7 <nxp3397@autuni.ac.nz>
2024-09-08 14:06:08 +1200

    d
    d

---
+++ Godot Project/Scenes/Tutorial/animation_player.gd
@@ -0,0 +1,38 @@
+extends AnimationPlayer
+
+var arrow_pressed = false
+var space_pressed = false
+var message_finished = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play("appear_arrow")
+	$Timer.connect("timeout", Callable(self, "_on_timer_timeout"))
+	$Timer2.connect("timeout", Callable(self, "_on_timer2_timeout"))
+	
+# Check if user pressed arrow key
+func _input(event):
+	if event.is_action_pressed("ui_right") and not arrow_pressed:
+		$Timer.start()
+		arrow_pressed = true
+	if event.is_action_pressed("ui_select") and not space_pressed and message_finished:
+		$Timer2.start()
+		space_pressed = true
+		
+func _on_timer_timeout():
+	play("fade_and_appear")
+	message_finished = true
+	
+func _on_timer2_timeout():
+	play("fade_space")
+	
+
+		
+
+		
+		
+	  
+	
+
+	
+	
