a1858c0fcc9188719716c411140862fccabdace4
Skye Pooley <tpb6375@autuni.ac.nz>
2024-09-16 17:14:51 +1200

    Created Template level
    Created a template level which new levels can inherit from. Ensures that all levels will have essential components like the pause menu, level transition, and backgrounds.
    
    Also added a texture for platforms.
    Started working on hub level.

---
+++ Godot Project/Scenes/Levels/template_level.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+# Signals connected to the game manager to control level loading.
+signal level_reset_requested;
+signal next_level_requested;
+signal main_menu_requested;
+
+@onready var pause_menu_popup: Window = $PauseMenuPopup
+
+# Connect all killzones to this method.
+func _on_player_died() -> void:
+	print("death relayed")
+	level_reset_requested.emit();
+
+# Connect the area2d level transition trigger to this method.
+func _on_level_transition_reached() -> void:
+	print("level transition relayed")
+	next_level_requested.emit();
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		pause_menu_popup.open();
+
+func _on_pause_menu_popup_main_menu_requested() -> void:
+	main_menu_requested.emit();
