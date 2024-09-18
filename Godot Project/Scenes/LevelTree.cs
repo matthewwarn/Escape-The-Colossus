776775e6d3a4347eb4673aeb5899bd4e2ac8654d
Matthew Warn <matthewryanwarn@gmail.com>
2024-09-18 12:55:26 +1200

    now actually up to date with main
    hub included

--- Godot Project/Scenes/LevelTree.cs
+++ Godot Project/Scenes/LevelTree.cs
@@ -2,13 +2,13 @@ using System.IO;
 
 public interface LevelTree
 {
-    string RootScenePath { get; }
-    string CurrentScenePath { get; }
-    
-    string TraverseUp();
-    string TraverseToChild(int childIndex);
-    string JumpToLevel(string path);
-    void Reset();
-    void Serialise(StreamWriter outputStream);
-    bool IsAtRoot();
+	string RootScenePath { get; }
+	string CurrentScenePath { get; }
+	
+	string TraverseUp();
+	string TraverseToChild(int childIndex);
+	string JumpToLevel(string path);
+	void Reset();
+	void Serialise(StreamWriter outputStream);
+	bool IsAtRoot();
 }
