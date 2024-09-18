e1a5900b2d7757b92e85dd23e55fed9d8efa7a8c
godofocean7 <nxp3397@autuni.ac.nz>
2024-09-19 03:33:44 +1200

    Update level_1.gd

--- Godot Project/Scenes/Levels/Level_1_Test/level_1.gd
+++ Godot Project/Scenes/Levels/Level_1_Test/level_1.gd
@@ -4,6 +4,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var player = get_node("Player")
+	# Lock double jump and dash since it is level one
 	player.double_jump_toggle = false
 	player.dash_toggle = false
 
