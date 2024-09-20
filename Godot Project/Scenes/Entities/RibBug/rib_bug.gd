55be2da33afad8502318d0f54e72f419abd3365a
Hendrik-Kruger <hendrik.b.k75@gmail.com>
2024-09-20 20:09:00 +1200

    added animation function to player and ribbug and got all the animations to work removed the jumping for ribbug as i decided it can be persued sprint 2 made it so that the ribbug does fall of  platforms  and that it waits till player enters  engagement area before starting to move and improved the movement
    made it so that it checks if it can chase player if it cant then it will just move left or right on the platform if the player has entered the engagement area before. added a function to make the ribbug instance go away when it dies after the death animation plays as well as for the player

--- Godot Project/Scenes/Entities/RibBug/rib_bug.gd
+++ Godot Project/Scenes/Entities/RibBug/rib_bug.gd
@@ -1,27 +1,28 @@
 extends CharacterBody2D
 
-const SPEED = 4000
-const JUMP_VELOCITY = -500.0
-const JUMP_BUFFER_TIME = 0.07
+const CHASE_SPEED = 4000          #this speed is for when chasing player
+const SPEED = 50                  #this speed is used when not chasing player
 
-var player_chase = false
+var player_chase = false          #tracks weither if enemy should chase player or not
 var player = null
 var health = 1
-var direction = -1
-var player_inattack_zone = false 
+var direction = -1                #this will be used for when enemy chases player
+var player_inattack_zone = false  #tracks weither player is in range to be attacked and to deal damage to enemy
 var can_take_damage = true
 var can_attack = true
 var is_alive = true
-var jump_buffer: bool = false
-var attack_ip = false
+var attack_ip = false             #tracks if enemy is attacking
+var facing = - 1                  #this is used when ribbug has engaged player but is no longer chasing player
+var has_engaged: bool = false     #tracks if player has engaged enemy 
+var can_chase: bool = true        #tracks if enemy can chase player 
 
 @onready var attack_cooldown = $attack_cooldown
 @onready var take_damage_cooldown = $take_damage_cooldown
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
-@onready var ray_cast_floor = $RayCastFloor
-@onready var ray_cast_floor_2 = $RayCastFloor2
+@onready var ray_cast_floor_left = $RayCastFloorLeft
+@onready var ray_cast_floor_right = $RayCastFloorRight
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -32,57 +33,94 @@ func _physics_process(delta):
 	attack()
 	enemy()
 	
-
+#when RibBug dies set is_alive and player_chase to false 
 	if health <= 0:
 		is_alive = false
 		player_chase = false
-
+	
+	#to enable gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta 
+	
+	# for when the player is in the RibBug's detection area 
 	if player_chase:
 		
 		if (direction) < 0:
 			animated_sprite.flip_h = false
 		else:
 			animated_sprite.flip_h = true
-
-		if ray_cast_floor.is_colliding() and ray_cast_floor_2.is_colliding():
+		
+		#if RibBug is on the floor
+		if ray_cast_floor_left.is_colliding() and ray_cast_floor_right.is_colliding():
 			
-			if ray_cast_left.is_colliding():
-				if velocity.y < 0 and is_on_floor():
-					velocity.y = JUMP_VELOCITY / 4
-					jump_buffer = true
-					#if ray_cast_left.is_colliding():
-						#direction = 1
-						#animated_sprite.flip_h = true
-
+			#can_chase = true 
+			
 			if ray_cast_right.is_colliding():
-				if velocity.y < 0 and is_on_floor():
-					velocity.y = JUMP_VELOCITY / 4
-					jump_buffer = true
-					#if ray_cast_right.is_colliding():
-						#direction = -1
-						#animated_sprite.flip_h = false
-
-			if not is_on_floor():
-				velocity.y += gravity * delta
-			else:
-				if jump_buffer:
-					velocity.y = JUMP_VELOCITY
-					jump_buffer = false
-
-			position.x += direction / (SPEED * delta )
-
-		elif ray_cast_floor.is_colliding() == false or ray_cast_floor_2.is_colliding() == false:
-			if (direction) < 0:
-				direction = 1
-				animated_sprite.flip_h = true
-			else:
-				direction = -1
 				animated_sprite.flip_h = false
+				facing = -1
+				position.x += facing * SPEED * delta
+			elif ray_cast_left.is_colliding():
+				animated_sprite.flip_h = true
+				facing = 1
+				position.x += facing * SPEED * delta 
+			else:
+				position.x += direction / (CHASE_SPEED * delta ) #this is for chasing the player
+		
+		elif ray_cast_floor_left.is_colliding() == false or ray_cast_floor_right.is_colliding() == false: #for when there is no floor
 			
-			position.x += direction * SPEED * delta 
+			can_chase = false
+			
+			if ray_cast_floor_left.is_colliding() == false and ray_cast_floor_right.is_colliding():
+				animated_sprite.flip_h = true
+				facing = 1
+				position.x += facing * SPEED * delta
+			elif ray_cast_floor_right.is_colliding() == false and ray_cast_floor_left.is_colliding():
+				animated_sprite.flip_h = false
+				facing = -1
+				position.x += facing * SPEED * delta
+	
+	elif has_engaged == true and player_chase == false: #if RibBug has engaged the player then it will move around on platform
+		
+		if (facing) < 0:
+			animated_sprite.flip_h = false
+			
+		else:
+			animated_sprite.flip_h = true
+			
+	 	
+		if ray_cast_floor_left.is_colliding() and ray_cast_floor_right.is_colliding():
+			
+			#can_chase = true
+			
+			if ray_cast_right.is_colliding():
+				animated_sprite.flip_h = false
+				facing = -1
+				position.x += facing * SPEED * delta
+			elif ray_cast_left.is_colliding():
+				animated_sprite.flip_h = true
+				facing = 1
+				position.x += facing * SPEED * delta 
+			
+			position.x += facing * SPEED * delta
+		
+		elif ray_cast_floor_left.is_colliding() == false or ray_cast_floor_right.is_colliding() == false:
+			
+			can_chase = false
+			
+			if ray_cast_floor_left.is_colliding() == false and ray_cast_floor_right.is_colliding():
+				animated_sprite.flip_h = true
+				facing = 1
+				position.x += facing * SPEED * delta
+			elif ray_cast_floor_right.is_colliding() == false and ray_cast_floor_left.is_colliding():
+				animated_sprite.flip_h = false
+				facing = -1
+				position.x += facing * SPEED * delta
+			
+		position.x += facing * SPEED * delta
 	
 	move_and_slide()
 
+#playing the different animations
 func play_animation():
 	if player_chase == false and is_alive == true :
 		animated_sprite.play("Idle")
@@ -92,29 +130,36 @@ func play_animation():
 		animated_sprite.play("Attack")
 	elif is_alive == false:
 		animated_sprite.play("Death ")
-		#self.queue_free()
+		
 
+# for when the player enters the engament area thus ribbug starts to chase
 func _on_detection_body_entered(body):
 	if body.has_method("player"):
 		player = body
-		player_chase = true
-		direction = (player.position.x - position.x)
+		if can_chase == true:
+			player_chase = true
+			direction = (player.position.x - position.x)
 
+# for when the player leaves the engament area
 func _on_detection_body_exited(body):
 	player = body
 	player = null
 	player_chase = false
+	has_engaged = true
 
+#for when player enters the area when the ribbug can take damage and attack
 func _on_hit_box_body_entered(body):
 	if body.has_method("player"):
 		player_inattack_zone = true 
 
+#for when player leaves the area when the ribbug can take damage and attack
 func _on_hit_box_body_exited(body):
 	if body.has_method("player"):
 		player_inattack_zone = false
 
+#this function is how the ribbug attacks 
 func attack():
-	var dir = direction
+	var dir = facing
 	if player_inattack_zone and can_attack:
 		attack_ip = true
 		if dir == 1:
@@ -128,6 +173,7 @@ func attack():
 			attack_cooldown.start()
 			attack_ip = false
 
+#this function is how to deal with damage taken
 func deal_with_damage():
 	if player_inattack_zone and globall.player_current_attack == true:
 		if can_take_damage == true:
@@ -136,12 +182,20 @@ func deal_with_damage():
 			can_take_damage = false
 			print("RibBug health: " + str(health))
 		
+
+#this is what happens when the timer that is for being able to recive damage times out
 func _on_take_damage_cooldown_timeout():
 	can_take_damage = true
 
-
+#this is what happens when the timer that is for the attack cooldown times out
 func _on_attack_cooldown_timeout():
 	can_attack = true
 
+#this is used by player to check if it is an enemy 
 func enemy():
 	pass
+
+#for when death animation is finnished to remove the instacnce of that ribbug
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite.animation == &"Death ":
+		self.queue_free()
