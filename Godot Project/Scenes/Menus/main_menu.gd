cc991cc936536b3b34818b4902bffd83697e3217
Skye Pooley <tpb6375@autuni.ac.nz>
2024-09-30 20:07:55 +1300

    Fullscreen can now be toggled from anywhere using F11

--- Godot Project/Scenes/Menus/main_menu.gd
+++ Godot Project/Scenes/Menus/main_menu.gd
@@ -5,6 +5,7 @@ signal game_resume_requested;
 
 @onready var resume_button: Button = $MainMenuParent/MarginContainer/VBoxContainer/ResumeButton
 @onready var settings_menu: Control = $SettingsMenu
+@onready var options_button: Button = $MainMenuParent/MarginContainer/VBoxContainer/OptionsButton
 
 func _ready() -> void:
 	# Starting point for keyboard navigation.
@@ -19,6 +20,12 @@ func _on_resume_button_pressed() -> void:
 func _on_options_button_pressed():
 	settings_menu.show();
 
-
 func _on_quit_button_pressed():
 	get_tree().quit();
+
+func _on_settings_menu_hidden() -> void:
+	options_button.grab_focus();
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("fullscreen"):
+		SettingsManager.toggle_fullscreen();
