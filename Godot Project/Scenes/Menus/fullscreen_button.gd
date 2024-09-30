bbf4631eebcaf57a5ac14fd5f78cd58e8708a95e
Skye Pooley <tpb6375@autuni.ac.nz>
2024-09-30 18:49:30 +1300

    Fullscreen toggle is now updated when settings menu opens

--- Godot Project/Scenes/Menus/fullscreen_button.gd
+++ Godot Project/Scenes/Menus/fullscreen_button.gd
@@ -1,5 +1,10 @@
 extends CheckButton
 
+func _update() -> void:
+	set_pressed_no_signal(
+		DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
+	);
+
 func _on_fullscreen_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN);
