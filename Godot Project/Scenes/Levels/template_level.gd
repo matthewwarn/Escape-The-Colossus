7cf4eec229b7286689efd44a2c21ef08eb5adc6b
Skye Pooley <131856802+SkyePooley@users.noreply.github.com>
2024-10-02 15:02:50 +1300

    New level linking system using export variables.

--- Godot Project/Scenes/Levels/template_level.gd
+++ Godot Project/Scenes/Levels/template_level.gd
@@ -2,13 +2,19 @@ extends Node2D
 
 # Signals connected to the game manager to control level loading.
 signal level_reset_requested;
-signal next_level_requested;
-signal previous_level_requested;
+signal level_requested(level_path: String);
 signal main_menu_requested;
 
 @onready var pause_menu_popup: Window = $PauseMenuPopup
 @onready var player: CharacterBody2D = $Player
 @onready var end_locator: Node2D = $EndLocator
+
+@export
+var previous_level: String;
+@export
+var exit_a: String;
+@export
+var exit_b: String;
 
 # Connect all killzones to this method.
 func _on_player_died() -> void:
@@ -16,13 +22,16 @@ func _on_player_died() -> void:
 	level_reset_requested.emit();
 
 # Connect the area2d level transition trigger to this method.
-func _on_level_transition_reached() -> void:
-	next_level_requested.emit();
+func _on_exit_a_reached() -> void:
+	level_requested.emit(exit_a);
+
+func _on_exit_b_reached() -> void:
+	level_requested.emit(exit_b);
 
 func _on_prev_level_transition() -> void:
-	previous_level_requested.emit()
+	level_requested.emit(previous_level, true);
 
-func _jump_to_end() -> void:
+func jump_to_end() -> void:
 	player.global_position = end_locator.global_position;
 
 func _unhandled_input(event: InputEvent) -> void:
---
+++ Godot Project/Scenes/game_manager.gd
@@ -0,0 +1,88 @@
+extends Node
+
+const GAME_SAVE_PATH: String = "Scenes/save.dat";
+const MAIN_MENU: String      = "Scenes/Menus/main_menu.tscn";
+const SETTINGS_MENU: String  = "Scenes/Menus/settings_menu.tscn";
+@export
+var FIRST_LEVEL: String;
+const LEVEL_ROOT_DIR: String = "Scenes/Levels/";
+
+var current_level: Node;
+var current_level_path: String;
+
+func _ready() -> void:
+	load_level(MAIN_MENU);
+
+# start_game
+func start_game() -> void:
+	load_level(FIRST_LEVEL);
+
+func resume_game() -> void:
+	current_level_path = read_save();
+	load_level(current_level_path);
+
+# open main menu
+func open_main_menu() -> void:
+	load_level(MAIN_MENU);
+
+# save game
+func save_game() -> void:
+	var save_data = {
+		"current_level": current_level_path
+	}
+	var serialised_data = JSON.stringify(save_data);
+	var file = FileAccess.open(GAME_SAVE_PATH, FileAccess.WRITE);
+	file.store_string(serialised_data);
+
+# read save
+func read_save() -> String:
+	var file = FileAccess.open(GAME_SAVE_PATH, FileAccess.READ);
+	var serialised_data = file.get_as_text();
+	var json = JSON.new();
+	var parse_error = json.parse(serialised_data);
+	if (parse_error == OK):
+		var save_data = json.data;
+		if (typeof(save_data) == TYPE_DICTIONARY):
+			return save_data["current_level"];
+		else:
+			print("save data corrupt.");
+	else:
+		print("Parse error in save file: ", json.get_error_message());
+	return FIRST_LEVEL;
+
+# load level
+func load_level(level_path: String, jump_to_end: bool = false) -> void:
+	call_deferred("load_level_deferred", level_path, jump_to_end);
+
+# Load a scene and connect all its signals
+func load_level_deferred(level_path: String, jump_to_end: bool = false) -> void:
+	current_level_path = level_path;
+	if (current_level != null):
+		remove_child(current_level);
+		current_level.call_deferred("free");
+	
+	var next_level_preload;
+	if (level_path == MAIN_MENU):
+		next_level_preload = load(level_path);
+	else:
+		next_level_preload = load(LEVEL_ROOT_DIR + level_path);
+	
+	current_level = next_level_preload.instantiate();
+	add_child(current_level);
+	#get_tree().current_scene = current_level;
+	
+	if (level_path == MAIN_MENU):
+		current_level.connect("game_start_requested", start_game);
+		current_level.connect("game_resume_requested", resume_game);
+	else:
+		save_game();
+		current_level.connect("level_reset_requested", reload_level);
+		current_level.connect("level_requested", load_level);
+		current_level.connect("main_menu_requested", open_main_menu);
+		if (jump_to_end):
+			current_level.jump_to_end();
+	
+
+# Reload current level on player death.
+func reload_level() -> void:
+	load_level(current_level_path);
