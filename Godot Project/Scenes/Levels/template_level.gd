64ece54433fa85107208d71abf618e1c8ecd2635
Skye Pooley <tpb6375@autuni.ac.nz>
2024-10-02 18:46:14 +1300

    Comments and formatting in new game manager and level scripts

--- Godot Project/Scenes/Levels/template_level.gd
+++ Godot Project/Scenes/Levels/template_level.gd
@@ -9,6 +9,8 @@ signal main_menu_requested;
 @onready var player: CharacterBody2D = $Player
 @onready var end_locator: Node2D = $EndLocator
 
+## Paths to adjacent game levels.
+## Give paths relative to res://Scenes/Levels and include .tscn
 @export
 var previous_level: String;
 @export
@@ -21,24 +23,30 @@ func _on_player_died() -> void:
 	print("death relayed")
 	level_reset_requested.emit();
 
-# Connect the area2d level transition trigger to this method.
+# Connect the area2d ExitA signal to this method.
 func _on_exit_a_reached() -> void:
 	level_requested.emit(exit_a);
 
+# Connect the area2d ExitB signal to this method.
 func _on_exit_b_reached() -> void:
 	level_requested.emit(exit_b);
 
+# Connect the area2d PrevLevelTransition signal to this method.
 func _on_prev_level_transition() -> void:
 	level_requested.emit(previous_level, true);
 
+# Move player to the end of the level.
+# Used when moving back to this level from the next level
 func jump_to_end() -> void:
 	player.global_position = end_locator.global_position;
 
+# Capture keypresses for fullscreen and pause.
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("fullscreen"):
 		SettingsManager.toggle_fullscreen();
 	if event.is_action_pressed("pause"):
 		pause_menu_popup.open();
 
+# Relay main menu request from pause menu.
 func _on_pause_menu_popup_main_menu_requested() -> void:
 	main_menu_requested.emit();
--- Godot Project/Scenes/game_manager.gd
+++ Godot Project/Scenes/game_manager.gd
@@ -3,29 +3,34 @@ extends Node
 const GAME_SAVE_PATH: String = "Scenes/save.dat";
 const MAIN_MENU: String      = "Scenes/Menus/main_menu.tscn";
 const SETTINGS_MENU: String  = "Scenes/Menus/settings_menu.tscn";
+
+## Path to first level of the game. Relative to LEVEL_ROOT_DIR
 @export
 var FIRST_LEVEL: String;
+## Path from which all level paths are given relative to.
 const LEVEL_ROOT_DIR: String = "Scenes/Levels/";
 
 var current_level: Node;
 var current_level_path: String;
 
+## When godot finishes loading game manager, load the main menu.
 func _ready() -> void:
 	load_level(MAIN_MENU);
 
-# start_game
+## Called when main menu new game button pressed.
 func start_game() -> void:
 	load_level(FIRST_LEVEL);
 
+## Called when main menu resume game button pressed.
 func resume_game() -> void:
 	current_level_path = read_save();
 	load_level(current_level_path);
 
-# open main menu
+## Return to main menu from pause menu in game.
 func open_main_menu() -> void:
 	load_level(MAIN_MENU);
 
-# save game
+## Save game state to file.
 func save_game() -> void:
 	var save_data = {
 		"current_level": current_level_path
@@ -34,12 +39,14 @@ func save_game() -> void:
 	var file = FileAccess.open(GAME_SAVE_PATH, FileAccess.WRITE);
 	file.store_string(serialised_data);
 
-# read save
+## Read previous game state from file.
+## returns path to current level in save.
 func read_save() -> String:
 	var file = FileAccess.open(GAME_SAVE_PATH, FileAccess.READ);
 	var serialised_data = file.get_as_text();
 	var json = JSON.new();
 	var parse_error = json.parse(serialised_data);
+	
 	if (parse_error == OK):
 		var save_data = json.data;
 		if (typeof(save_data) == TYPE_DICTIONARY):
@@ -50,13 +57,17 @@ func read_save() -> String:
 		print("Parse error in save file: ", json.get_error_message());
 	return FIRST_LEVEL;
 
-# load level
+## Load Level
+##
+## Calls to deferred method, no need to call this method deferred.
+## [param jump_to_end] Whether to move player to the end of the new level.
 func load_level(level_path: String, jump_to_end: bool = false) -> void:
-	call_deferred("load_level_deferred", level_path, jump_to_end);
+	call_deferred("_load_level_deferred", level_path, jump_to_end);
 
-# Load a scene and connect all its signals
-func load_level_deferred(level_path: String, jump_to_end: bool = false) -> void:
+## Load a scene and connect all its signals
+func _load_level_deferred(level_path: String, jump_to_end: bool = false) -> void:
 	current_level_path = level_path;
+	## Needed as when this is called in _ready there is no loaded level.
 	if (current_level != null):
 		remove_child(current_level);
 		current_level.call_deferred("free");
@@ -69,8 +80,8 @@ func load_level_deferred(level_path: String, jump_to_end: bool = false) -> void:
 	
 	current_level = next_level_preload.instantiate();
 	add_child(current_level);
-	#get_tree().current_scene = current_level;
 	
+	# Connect signals
 	if (level_path == MAIN_MENU):
 		current_level.connect("game_start_requested", start_game);
 		current_level.connect("game_resume_requested", resume_game);
@@ -79,6 +90,7 @@ func load_level_deferred(level_path: String, jump_to_end: bool = false) -> void:
 		current_level.connect("level_reset_requested", reload_level);
 		current_level.connect("level_requested", load_level);
 		current_level.connect("main_menu_requested", open_main_menu);
+		# When moving to a previous level, we need to jump to the end of the level.
 		if (jump_to_end):
 			current_level.jump_to_end();
 	
