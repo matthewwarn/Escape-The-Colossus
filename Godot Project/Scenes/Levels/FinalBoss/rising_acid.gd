776de12d6906d6ba7eaf927050688c1de9ebfb54
Matthew Warn <matthewryanwarn@gmail.com>
2024-10-03 16:37:29 +1300

    heart can be destroyed
    breaks block, makes acid appear.

--- Godot Project/Scenes/Levels/FinalBoss/rising_acid.gd
+++ Godot Project/Scenes/Levels/FinalBoss/rising_acid.gd
@@ -1,38 +1,32 @@
 extends Node2D
 
-var speed: int = 25
+var speed: int = 20
 var direction = Vector2(0, -1)
 var heart_hits: int = 0;
 var acid_active = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass 
+	position.x = 5000
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	activate_acid(delta)
-	
-func activate_acid(delta):
-	#check for heart attack
-	#play squishy blood sfx
-	#heart_hits++
-	
-	#When heart is destroyed
-	if(heart_hits == 3){
-		#disable heart
-		#play monster roar sfx
-		#enable acid
-		acid_active = true
-	}
-	
-	if(acid_active == true):
+	if acid_active:
 		rise_acid(delta)
 
+func _on_area_2d_heart_destroyed_signal():
+	#play monster roar sfx
+	#destroy tunnel blockage
+	#enable acid
+	#play escape music
+	acid_active = true;
+
 func rise_acid(delta):
+	position.x = 0
+	
 	#when reach bottom of tunnel, speed up
 	if(position.y <= -200):
-		speed = 55
+		speed = 57
 	
 	#move until it reaches the skull
 	if(position.y >= -2564):
