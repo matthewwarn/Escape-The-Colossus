61046a7eead646fd8bf639a919b875a4944b2fa8
godofocean7 <nxp3397@autuni.ac.nz>
2024-10-11 16:18:24 +1300

    Blood splash particle system added, level three edited

--- Godot Project/Scenes/Levels/Level Two/popup.gd
+++ Godot Project/Scenes/Levels/Level Two/popup.gd
@@ -4,6 +4,8 @@ extends AnimationPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	play("level two appear")
+	
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- Godot Project/Scenes/Levels/Level Two/press z.gd
+++ Godot Project/Scenes/Levels/Level Two/press z.gd
@@ -3,7 +3,9 @@ extends AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	play("level two appear")
+	pass
+	# play("press z appear")
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
