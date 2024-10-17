070812ddb630f4d46120913810be8f5e9394b868
Hendrik-Kruger <hendrik.b.k75@gmail.com>
2024-10-17 14:37:51 +1300

    modified blood splash particle to be able to integrate it with ribbug
    changed the root node form normal node to node2D which attaches it to the ribbug or entity you add the scene to also added some code to control when the effect happens

---
+++ Godot Project/Scenes/Particle System/node_2d.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+@onready var particles = $CPUParticles2D
+@onready var area_2d = $Area2D
+
+func is_emitting():
+	particles.emitting = true
+	
+
+
+func is_not_emitting():
+	particles.emitting = false
